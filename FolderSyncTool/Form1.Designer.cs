c6f34b48daaf54eb091dc119a1ae1081766a2105
Tobby Li <tobby.li@festo.com>
2020-04-23 17:04:38 +0800

    first commit

---
+++ FolderSyncTool/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace FolderSyncTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "源目录：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(124, 198);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "目标目录：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(134, 27);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(473, 30);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(215, 191);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(158, 30);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "172.16.141.110";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(615, 26);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "路径选择";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(381, 253);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 33);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "上传";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(381, 191);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(305, 30);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "/media/0x00000020.1/";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(29, 17);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(769, 151);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 32);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Size = new System.Drawing.Size(761, 115);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "按路径上传";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(4, 4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(753, 107);
+            this.panel1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.panel2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 32);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Size = new System.Drawing.Size(761, 115);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "指定文件上传";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(4, 4);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(753, 107);
+            this.panel2.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(622, 34);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "文件选择";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(19, 17);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(586, 64);
+            this.textBox4.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 303);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("MetaPlusLF", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ FolderSyncTool/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.IO;
+
+namespace FolderSyncTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] FilesPath;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {                
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+                FileInfo[] files = new DirectoryInfo(folderBrowserDialog1.SelectedPath).GetFiles();
+                FilesPath = new string[files.Length];
+                for (int i = 0; i < files.Length; i++)
+                {
+                    FilesPath[i] = files[i].FullName;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                FilesPath = openFileDialog1.FileNames;
+                textBox1.Text = System.IO.Path.GetDirectoryName(openFileDialog1.FileName);
+                textBox4.Text="";
+                for (int i = 0; i < openFileDialog1.SafeFileNames.Length; i++)
+                {
+                    textBox4.Text = textBox4.Text + "\"" + openFileDialog1.SafeFileNames[i] + "\"";
+                }
+                
+            }
+        }
+
+        private static void EndGetStreamCallback(IAsyncResult ar)
+        {
+            FtpState state = (FtpState)ar.AsyncState;
+
+            Stream requestStream = null;
+            // End the asynchronous call to get the request stream.
+            try
+            {
+                requestStream = state.Request.EndGetRequestStream(ar);
+                // Copy the file contents to the request stream.
+                const int bufferLength = 2048;
+                byte[] buffer = new byte[bufferLength];
+                int count = 0;
+                int readBytes = 0;
+                FileStream stream = File.OpenRead(state.FileName);
+                do
+                {
+                    readBytes = stream.Read(buffer, 0, bufferLength);
+                    requestStream.Write(buffer, 0, readBytes);
+                    count += readBytes;
+                }
+                while (readBytes != 0);
+                Console.WriteLine("Writing {0} bytes to the stream.", count);
+                // IMPORTANT: Close the request stream before sending the request.
+                requestStream.Close();
+                // Asynchronously get the response to the upload request.
+                state.Request.BeginGetResponse(
+                    new AsyncCallback(EndGetResponseCallback),
+                    state
+                );
+            }
+            // Return exceptions to the main application thread.
+            catch (Exception e)
+            {
+                Console.WriteLine("Could not get the request stream.");
+                state.OperationException = e;
+                state.OperationComplete.Set();
+                return;
+            }
+        }
+
+        // The EndGetResponseCallback method  
+        // completes a call to BeginGetResponse.
+        private static void EndGetResponseCallback(IAsyncResult ar)
+        {
+            FtpState state = (FtpState)ar.AsyncState;
+            FtpWebResponse response = null;
+            try
+            {
+                response = (FtpWebResponse)state.Request.EndGetResponse(ar);
+                response.Close();
+                state.StatusDescription = response.StatusDescription;
+                // Signal the main application thread that 
+                // the operation is complete.
+                state.OperationComplete.Set();
+            }
+            // Return exceptions to the main application thread.
+            catch (Exception e)
+            {
+                Console.WriteLine("Error getting response.");
+                state.OperationException = e;
+                state.OperationComplete.Set();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Create a Uri instance with the specified URI string.
+            // If the URI is not correctly formed, the Uri constructor
+            // will throw an exception.
+            ManualResetEvent waitObject;
+            for (int i = 0; i < FilesPath.Length; i++)
+            {
+                Uri target = new Uri(@"ftp://" + textBox2.Text + textBox3.Text + System.IO.Path.GetFileName(FilesPath[i]));
+                string fileName = FilesPath[i];
+                FtpState state = new FtpState();
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(target);
+                request.Method = WebRequestMethods.Ftp.UploadFile;
+
+                // This example uses anonymous logon.
+                // The request is anonymous by default; the credential does not have to be specified. 
+                // The example specifies the credential only to
+                // control how actions are logged on the server.
+
+                request.Credentials = new NetworkCredential("root", "Festo");
+
+                // Store the request in the object that we pass into the
+                // asynchronous operations.
+                state.Request = request;
+                state.FileName = fileName;
+
+                // Get the event to wait on.
+                waitObject = state.OperationComplete;
+
+                // Asynchronously get the stream for the file contents.
+                request.BeginGetRequestStream(
+                    new AsyncCallback(EndGetStreamCallback),
+                    state
+                );
+
+                // Block the current thread until all operations are complete.
+                waitObject.WaitOne();
+
+                // The operations either completed or threw an exception.
+                if (state.OperationException != null)
+                {
+                    //throw state.OperationException;
+                }
+                else
+                {
+                    MessageBox.Show("The operation completed - {0}", state.StatusDescription);
+                }
+            }
+            
+        }
+    }
+
+    public class FtpState
+    {
+        private ManualResetEvent wait;
+        private FtpWebRequest request;
+        private string fileName;
+        private Exception operationException = null;
+        string status;
+
+        public FtpState()
+        {
+            wait = new ManualResetEvent(false);
+        }
+
+        public ManualResetEvent OperationComplete
+        {
+            get { return wait; }
+        }
+
+        public FtpWebRequest Request
+        {
+            get { return request; }
+            set { request = value; }
+        }
+
+        public string FileName
+        {
+            get { return fileName; }
+            set { fileName = value; }
+        }
+        public Exception OperationException
+        {
+            get { return operationException; }
+            set { operationException = value; }
+        }
+        public string StatusDescription
+        {
+            get { return status; }
+            set { status = value; }
+        }
+    }
+}
